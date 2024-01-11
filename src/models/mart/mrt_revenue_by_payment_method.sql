b30753af9f7e45509447dcb6c5df999435b8ebd9
Rob de <rob.dewit@y42.com>
2024-01-11 12:22:44 +0100

    model: exclude payment_method=Null from results table

--- src/models/mart/mrt_revenue_by_payment_method.sql
+++ src/models/mart/mrt_revenue_by_payment_method.sql
@@ -20,6 +20,10 @@ FROM stg_orders AS ord
 LEFT JOIN stg_payments AS pay
 	ON ord.order_id = pay.order_id
 
+WHERE
+	TRUE
+	AND pay.payment_method IS NOT NULL
+
 GROUP BY order_date, pay.payment_method
 	
 ORDER BY order_date DESC, pay.payment_method ASC
