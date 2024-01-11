cd6ea9f4098caf5b00b972a41943f5c4d0b33ef9
Octavian Zarzu <octavian.zarzu@y42.com>
2024-01-11 15:29:04 +0200

    fillnan values

--- src/python_ingest/raw_open_exoplanet_catalogue.py
+++ src/python_ingest/raw_open_exoplanet_catalogue.py
@@ -8,5 +8,6 @@ from y42.v1.decorators import data_loader
 def exoplanets() -> pd.DataFrame:
     file_exoplanets = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/oec_tables/master/comma_separated/open_exoplanet_catalogue.txt"
     df = pd.read_csv(file_exoplanets)
+    df.fillna('', inplace=True)
 
     return df
