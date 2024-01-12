5c10c733aad541cab02ea84aa8e727522dc57738
Octavian Zarzu <octavian.zarzu@y42.com>
2024-01-12 11:34:29 +0200

    checkpoint

--- src/python_ingest/raw_open_exoplanet_catalogue.py
+++ src/python_ingest/raw_open_exoplanet_catalogue.py
@@ -6,13 +6,16 @@ import logging
 from y42.v1.decorators import data_loader
 
 @data_loader
-def exoplanets() -> pd.DataFrame:
+def exoplanets(context) -> pd.DataFrame:
     file_exoplanets = "https://raw.githubusercontent.com/OpenExoplanetCatalogue/oec_tables/master/comma_separated/open_exoplanet_catalogue.txt"
-    
-    r = requests.get(file_exoplanets, auth=('user', 'pass'))
+    user = context.secrets.get("EXOPLANET_USER")
+    password = context.secrets.get("EXOPLANET_KEY")
+
+    r = requests.get(file_exoplanets, auth=(user, password))
     if r.status_code == 200:
         data = StringIO(r.content.decode('utf-8'))
         df = pd.read_csv(data)
+        logging.info(password)
     else: 
         logging.error(f"An error occurred. Error status_code: {r.status_code}")
     
