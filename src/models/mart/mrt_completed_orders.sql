a77ee3fdd5ce52a1d676a2ad3d4003d9a8782496
Octavian Zarzu <octavian.zarzu@y42.com>
2024-01-17 21:10:55 +0200

    checkpoint

--- src/models/mart/mrt_completed_orders.sql
+++ src/models/mart/mrt_completed_orders.sql
@@ -18,10 +18,10 @@ stg_exoplanets AS (
 
 
 SELECT
-	DISTINCT 1 as x1, 2 as x2, 3 as x3, 4 as x4, 5 as x5, 6 as x6, 7 as x7, 8 as x8, 9 as x9, 10 as x10, 11 as x12, 13 as x13
+	DISTINCT 1 as x1, 2 as x2, 3 as x3, 4 as x4, 5 as x5, 6 as x6, 7 as x7, 8 as x8, 9 as x9, 10 as x10, 11 as x12, 13 as x13, 14 as x14
 FROM stg_orders
 UNION ALL 
 SELECT 
-    DISTINCT 1 as x1, 2 as x2, 3 as x3, 4 as x4, 5 as x5, 6 as x6, 7 as x7, 8 as x8, 9 as x9, 10 as x10, 11 as x12, 13 as x13
+    DISTINCT 1 as x1, 2 as x2, 3 as x3, 4 as x4, 5 as x5, 6 as x6, 7 as x7, 8 as x8, 9 as x9, 10 as x10, 11 as x12, 13 as x13, 14 as x14
 FROM stg_exoplanets
 
