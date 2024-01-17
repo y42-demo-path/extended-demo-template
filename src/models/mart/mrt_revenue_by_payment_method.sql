1dd7a26a7e1e27084c6d392627fdec39e54598e5
Octavian Zarzu <octavian.zarzu@y42.com>
2024-01-17 15:52:13 +0200

    change payment method

--- src/models/mart/mrt_revenue_by_payment_method.sql
+++ src/models/mart/mrt_revenue_by_payment_method.sql
@@ -17,12 +17,8 @@ SELECT
 
 FROM stg_orders AS ord
 
-LEFT JOIN stg_payments AS pay
+INNER JOIN stg_payments AS pay
 	ON ord.order_id = pay.order_id
-
-WHERE
-	TRUE
-	AND pay.payment_method IS NOT NULL
 
 GROUP BY order_date, pay.payment_method
 	
