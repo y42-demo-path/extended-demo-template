4a9c973a5f8e9c2c4c94ffe637651f6ee7ae5863
Rob de Wit <rob.dewit@y42.com>
2024-02-08 11:16:54 +0100

    transform: remove unneeded timestamp conversion

--- src/models/staging/stg_customer_loyalty.sql
+++ src/models/staging/stg_customer_loyalty.sql
@@ -7,7 +7,7 @@ source AS (
 renamed AS (
 	SELECT
 		`Customer_ID` AS customer_id,
-		parse_timestamp('%FT%TZ', `Member_since`) AS member_since,
+		Member_since AS member_since,
 		cast(source.`Points` AS int) AS total_points,
 		tier AS tier
 	FROM source
