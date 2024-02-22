d48363ef2aecc8eeab8b0f666cbb2194f3939188
Rob de Wit <rob.dewit@y42.com>
2024-02-22 11:56:33 +0100

    ingest: fix unzip in Kaggle response

--- src/python_ingest/space_missions_1957.py
+++ src/python_ingest/space_missions_1957.py
@@ -2,30 +2,9 @@ import base64
 import requests
 import pandas as pd
 import logging
-import json
+import io
 
-from io import StringIO
 from y42.v1.decorators import data_loader
-
-# @data_loader
-# def space_missions_1957(context) -> pd.DataFrame:
-
-#     kaggle_username = context.secrets.get('kaggle_username') # get the value of a specific secret saved within this space
-#     kaggle_password = context.secrets.get('kaggle_password') # get the value of a specific secret saved within this space
-
-#     kaggle_info = {'UserName': kaggle_username, 'Password': kaggle_password} # Kaggle Username and Password
-
-#     url = "https://www.kaggle.com/datasets/agirlcoding/all-space-missions-from-1957/download?datasetVersionNumber=6"
-#     response = requests.get(url, data = kaggle_info)# ,prefetch = False) # Login to Kaggle and retrieve the data.
-    
-#     if response.status_code == 200:
-#         data = StringIO(requests.content.decode('utf-8'))
-#         df = pd.read_csv(data)
-#         logging.info("Data fetched and DataFrame created successfully.")
-#     else: 
-#         logging.error(f"An error occurred. Error status_code: {requests.status_code}")
-    
-#     return df
 
 @data_loader
 def space_missions_1957(context) -> pd.DataFrame:
@@ -48,10 +27,9 @@ def space_missions_1957(context) -> pd.DataFrame:
     #3: Sending a GET request to the URL with the encoded credentials.
     response = requests.get(url, headers=headers)
 
-    #4: Read response into dataframe
+    #4: Unzip response and read into dataframe
     if response.status_code == 200:
-        data = response.content.decode('utf-8'))
-        df = pd.read_csv(data, encoding='unicode_escape')
+        df = pd.read_csv(io.BytesIO(response.content), compression="zip")
         logging.info("Data fetched and DataFrame created successfully.")
     else: 
         logging.error(f"An error occurred. Error status_code: {requests.status_code}")
